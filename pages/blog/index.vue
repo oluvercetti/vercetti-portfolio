d7af0cde46747745d15c9bd144300b10464d0417
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2022-10-31 17:07:41 +0100

    added content to blog and works view

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,39 @@
 <template>
-    <div />
+    <main>
+        <div class="blogpage__container">
+            <h1 class="blogpage__header">
+                Blog
+            </h1>
+            <Post
+                title="Making a design system from scratch"
+                date="12 Feb, 2022"
+                subtitle="Design, Pattern"
+                body="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+            />
+            <hr class="work__divider">
+            <Post
+                title="Creating pixel perfect icons in Figma"
+                date="12 Feb, 2022"
+                subtitle="Figma, Icon Design"
+                body="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+            />
+            <hr class="work__divider">
+            <Post
+                title="Creating pixel perfect icons in Figma"
+                date="12 Feb, 2022"
+                subtitle="Figma, Icon Design"
+                body="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+            />
+            <hr class="work__divider">
+            <Post
+                title="Creating pixel perfect icons in Figma"
+                date="12 Feb, 2022"
+                subtitle="Figma, Icon Design"
+                body="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+            />
+            <hr class="blogpage__divider">
+        </div>
+    </main>
 </template>
 
 <script>
@@ -11,5 +45,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.posts {
+    &__row {
+        min-height: unset;
+        padding: unset;
+    }
 
+    &__details {
+        width: unset;
+    }
+}
 </style>
